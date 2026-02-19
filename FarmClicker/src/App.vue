dd3d7a187c26c99e017d85a6ed9055202cd4e5fc
Bruno <filobruno2@gmail.com>
2026-02-19 11:59:20 +0100

    Create Vite app and basic html

---
+++ FarmClicker/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <nav>
+    <header>
+        <h1>Farm Clicker</h1>
+        <div><p>gold: --var--</p></div>
+        <div><p>time per click: --var--</p></div>
+    </header>
+  </nav>
+  <main>
+    <section>
+      <div><p>achievements</p></div>
+      <div><p>settings</p></div>
+    </section>
+    <section>
+      <div>field1</div>
+      <div>field2</div>
+      <div>field3</div>
+    </section>
+    <section>
+      <div><p>shop</p></div>
+      <div><p>crops</p></div>
+      <div><p>upgrades</p></div>
+      <div><p>market</p></div>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
